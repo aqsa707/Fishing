6879e028162d0aa9c3cc8bf248eeb146c36ac4a8
aqsa707 <146213719+aqsa707@users.noreply.github.com>
2024-02-09 10:10:33 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,143 @@
+// Completed Fish Scene JS
+
+// Canvas - Fishing Scene
+
+// Canvas Setup
+var cnv = document.getElementById("myCanvas");
+var ctx = cnv.getContext("2d");
+cnv.width = 400;
+cnv.height = 400;
+
+// top-left corner (0,0)
+// top-right corner (400,0)
+// bottom-left corner (0,400)
+// bottom-right corner (400,400)
+
+// Sky
+ctx.fillStyle = "lightblue";
+ctx.fillRect(0, 0, 400, 400);
+
+// Water
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 250, 400, 150);
+
+// Pier landing
+ctx.fillStyle = "brown";
+ctx.fillRect(0, 200, 250, 25);
+
+// Pier posts
+ctx.lineWidth = 10;
+ctx.strokeStyle = "brown";
+
+ctx.beginPath();
+ctx.moveTo(10, 150);
+ctx.lineTo(10, 400);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(60, 150);
+ctx.lineTo(60, 400);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(110, 150);
+ctx.lineTo(110, 400);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(160, 150);
+ctx.lineTo(160, 400);
+ctx.stroke();
+
+// Sun
+ctx.fillStyle = "yellow";
+ctx.beginPath();
+ctx.arc(75, 50, 20, 0, 2 * Math.PI);
+ctx.fill();
+
+// Fish Head 1
+ctx.fillStyle = "green";
+ctx.beginPath();
+ctx.arc(220, 300, 10, 0, 2 * Math.PI);
+ctx.fill();
+
+// Fish Tail 1
+ctx.beginPath();
+ctx.moveTo(220, 300);
+ctx.lineTo(240, 290);
+ctx.lineTo(240, 310);
+ctx.lineTo(220, 300);
+ctx.fill();
+
+// Fish Head 2
+ctx.fillStyle = "cyan";
+ctx.beginPath();
+ctx.arc(300, 325, 10, 0, 2 * Math.PI);
+ctx.fill();
+
+// Fish Tail 2
+ctx.beginPath();
+ctx.moveTo(300, 325);
+ctx.lineTo(320, 315);
+ctx.lineTo(320, 335);
+ctx.lineTo(300, 325);
+ctx.fill();
+
+// Stickman
+
+// Head - stroke circle (arc) at (250,140) with radius 15
+ctx.strokeStyle = "black";
+ctx.lineWidth = 3;
+ctx.beginPath();
+ctx.arc(250, 140, 15, 0, 2 * Math.PI);
+ctx.stroke();
+
+// Torso
+ctx.beginPath();
+ctx.moveTo(248, 155);
+ctx.lineTo(243, 195);
+ctx.stroke();
+
+// Upper Leg
+ctx.beginPath();
+ctx.moveTo(243, 195);
+ctx.lineTo(265, 205);
+ctx.stroke();
+
+// Lower Leg
+ctx.beginPath();
+ctx.moveTo(265, 205);
+ctx.lineTo(270, 230);
+ctx.stroke();
+
+// Arm
+ctx.beginPath();
+ctx.moveTo(246, 170);
+ctx.lineTo(280, 180);
+ctx.stroke();
+
+// Fishing Pole
+ctx.strokeStyle = "gray";
+ctx.lineWidth = 5;
+ctx.beginPath();
+ctx.moveTo(275, 190);
+ctx.lineTo(350, 100);
+ctx.stroke();
+
+// Fishing line
+ctx.lineWidth = 1;
+ctx.beginPath();
+ctx.moveTo(350, 100);
+ctx.lineTo(350, 300);
+ctx.stroke();
+
+// Cloud Image
+let cloud = document.getElementById("cloudImg");
+ctx.drawImage(cloud, 100, 50, 75, 75);
+ctx.drawImage(cloud, 200, 20, 75, 75);
+ctx.drawImage(cloud, 150, 100, 75, 75);
+
+// Text
+ctx.font = "15px Arial";
+ctx.fillStyle = "white";
+ctx.fillText("Art by J.Bacque", 275, 385);
